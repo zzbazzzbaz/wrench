273a46e92aa34d1c195a4142419b538b97b73e66
gqt <3217233537@qq.com>
2025-09-18 14:16:12 +0800

    限流组建

--- wrench-starter-rate-limiter/src/main/java/top/chatgqt/wrench/rate/limiter/config/RateLimiterAutoConfig.java
+++ wrench-starter-rate-limiter/src/main/java/top/chatgqt/wrench/rate/limiter/config/RateLimiterAutoConfig.java
@@ -1,6 +1,5 @@
 package top.chatgqt.wrench.rate.limiter.config;
 
-import com.google.common.util.concurrent.RateLimiter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import top.chatgqt.wrench.rate.limiter.aop.RateLimiterAOP;
